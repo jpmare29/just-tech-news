e90ab6275099b909edb91a151a656b199f333384
jpmare29 <jpmmare@yahoo.com>
2022-05-22 18:00:11 -0400

    fixed typo, updated sql tables

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,7 @@
-const { Model, DataTypes } = require('sequilize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {
-    //just to push
-}
+class Post extends Model {}
 
 Post.init(
     {
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const res = require('express/lib/response');
 const { User } = require('../../models');
 
 // GET /api/users
