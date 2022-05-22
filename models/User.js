70f69bca820ec35655e5b684c9e4c06a2cc8ec34
jpmare29 <jpmmare@yahoo.com>
2022-05-22 10:05:50 -0400

    validated password

--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
     {
@@ -43,6 +48,17 @@ User.init(
       }
     },
     {
+      hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+          async beforeCreate(newUserData) {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+          },
+          async beforeUpdate(updatedUserData) {
+              updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+              return updatedUserData;
+          }
+      },
       // pass in our imported sequelize connection (the direct connection to our database)
       sequelize,
       // don't automatically create createdAt/updatedAt timestamp fields
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const res = require('express/lib/response');
 const { User } = require('../../models');
 
 // GET /api/users
@@ -49,6 +50,28 @@ router.post('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!'});
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  })
+})
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
